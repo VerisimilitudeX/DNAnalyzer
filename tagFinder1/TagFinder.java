b634304fafe040d6242681c97a93f81e9e5a7372
Piyush Acharya <acharyapiyush1@outlook.com>
2022-07-31 17:29:38 -0700

    letters => nucleotides
    Made the comments more specific

--- tagFinder1/TagFinder.java
+++ tagFinder1/TagFinder.java
@@ -1,7 +1,7 @@
 /*
- * Finds a protein within a strand of DNA represented as a string of c,g,t,a letters.
+ * Finds a protein within a strand of DNA represented as a string of a,t,g,c nucleotides.
  * A protein is a part of the DNA strand marked by start and stop codons (DNA triples)
- * that is a multiple of 3 letters long.
+ * that is a multiple of 3 nucleotides long.
  */
 import edu.duke.*;
 import java.io.*;
