af989a51fdb365d47540fb7964a6e5ac739d4eba
Piyush Acharya <acharyapiyush1@outlook.com>
2022-08-01 14:06:47 -0700

    Added data from DNA codon table

--- StringsFirstAssignments/StringsFirstAssignments/Part2.java
+++ StringsFirstAssignments/StringsFirstAssignments/Part2.java
@@ -1,4 +1,5 @@
 import java.util.*;
+import edu.duke.*;
 
 public class Part2 {
     public String findSimpleGene(String dna, String startCodon, String stopCodon) {
@@ -29,14 +30,35 @@ public class Part2 {
         dnaList.add("GCTACATGGTACGTACGTAATAGC");
         dnaList.add("GATGCTATATAATAGTTATATAGC");
         dnaList.add("GGCTATGATATATGATATAGC");
+        dnaList.add("ATGATATATATATATATATAGC");
+
+        /* All 64 potential three-letter combinations of the DNA coding units T, C, A, and G are employed to
+        * encode one of these amino acids or to signify the end of a sequence. While DNA can be decoded without 
+        * ambiguity, a DNA sequence cannot be predicted from its protein sequence. Because most amino acids have 
+        * numerous codons, several DNA sequences might represent the same protein sequence.
+        */
 
         ArrayList<String> Isoleucine = new ArrayList<>(Arrays.asList("ATT", "ATC", "ATA"));
-        ArrayList<String> Leucine = new ArrayList<>(Arrays.asList("ATT", "ATC", "ATA"));
-
-        List<String> stopCodonList= new ArrayList<>();
-        stopCodonList.add("TAA");
-        stopCodonList.add("TAG");
-        stopCodonList.add("TGA");
+        ArrayList<String> Leucine = new ArrayList<>(Arrays.asList("CTT", "CTC", "CTA", "CTG", "TTA", "TTG"));
+        ArrayList<String> Valine = new ArrayList<>(Arrays.asList("GTT", "GTC", "GTA", "GTG"));
+        ArrayList<String> Phenylalanine = new ArrayList<>(Arrays.asList("TTT", "TTC"));
+        ArrayList<String> Methionine = new ArrayList<>(Arrays.asList("ATG"));
+        ArrayList<String> Cysteine = new ArrayList<>(Arrays.asList("TGT", "TGC"));
+        ArrayList<String> Alanine = new ArrayList<>(Arrays.asList("GCT", "GCC", "GCA", "GCG"));
+        ArrayList<String> Glycine = new ArrayList<>(Arrays.asList("GGT", "GGC", "GGA", "GGG"));
+        ArrayList<String> Proline = new ArrayList<>(Arrays.asList("CCT", "CCC", "CCA", "CCG"));
+        ArrayList<String> Threonine = new ArrayList<>(Arrays.asList("ACT", "ACC", "ACA", "ACG"));
+        ArrayList<String> Serine = new ArrayList<>(Arrays.asList("TCT", "TCC", "TCA", "TCG", "AGT", "AGC"));
+        ArrayList<String> Tyrosine = new ArrayList<>(Arrays.asList("TAT", "TAC"));
+        ArrayList<String> Tryptophan = new ArrayList<>(Arrays.asList("TGG"));
+        ArrayList<String> Glutamine = new ArrayList<>(Arrays.asList("CAA", "CAG"));
+        ArrayList<String> Asparagine = new ArrayList<>(Arrays.asList("AAT", "AAC"));
+        ArrayList<String> Histidine = new ArrayList<>(Arrays.asList("CAT", "CAC"));
+        ArrayList<String> GlutamicAcid = new ArrayList<>(Arrays.asList("GAA", "GAG"));
+        ArrayList<String> AsparticAcid = new ArrayList<>(Arrays.asList("GAT", "GAC"));
+        ArrayList<String> Lysine = new ArrayList<>(Arrays.asList("AAA", "AAG"));
+        ArrayList<String> Arginine = new ArrayList<>(Arrays.asList("CGT", "CGC", "CGA", "CGG", "AGA", "AGG"));
+        ArrayList<String> Stop = new ArrayList<>(Arrays.asList("TAA", "TAG", "TGA"));
         
 
         // ask user for the amino acid that they want to search for in the DNA strand, using the corresponding amino acid lists above.
