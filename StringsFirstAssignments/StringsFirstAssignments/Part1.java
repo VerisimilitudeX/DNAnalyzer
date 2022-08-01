93c52a7acf10cdadf56319137da196765e99d888
Piyush Acharya <acharyapiyush1@outlook.com>
2022-07-31 18:59:02 -0700

    Fixed bugs present in the previous version of the software.

--- StringsFirstAssignments/StringsFirstAssignments/Part1.java
+++ StringsFirstAssignments/StringsFirstAssignments/Part1.java
@@ -4,32 +4,35 @@ class Part1 {
     public String findSimpleGene(String dna) {
         int startIndex = dna.indexOf("ATG");
         if (startIndex == -1) {
-            return "";
+            return "Gene not found";
         }
         int stopIndex = dna.indexOf("TAA");
         if (stopIndex == -1) {
-            return "";
+            return "Gene not found";
         }
         if (((stopIndex - startIndex + 1) % 3) == 0) {
             String gene = "Gene is " + dna.substring(startIndex, stopIndex + 3);
             return gene;
         }
+        else {
+            return "Gene not found";
+        }
     }
 
     public void testSimpleGene() {
         List<String> dnaList = new ArrayList<>();
-        dnaList.add("ATGGCTATATATATATATAGC");
-        dnaList.add("GCATTAATTAATTAATAATGC");
-        dnaList.add("GCGCATGCATGCATGCGC");
+        dnaList.add("ATGGCTATATATAATATATAGC");
+        dnaList.add("ATGCATTAATTAATTAATAATGC");
+        dnaList.add("GCGCATGCATGCATGCTAAGC");
         dnaList.add("GATGCCTAATGGCTAGCTATAAGCTGC");
         dnaList.add("GCATGCGCGCGCGCGCTAAGCGGC");
-        dnaList.add("GCGCCGGCCATGTAAGGCCGGCGC");
+        dnaList.add("GCGCCGGCCATGGATCTAAGGCCGGCGC");
         dnaList.add("GCTACATGGTACGTACGTAATAGC");
         dnaList.add("GATGCTATATAATAGTTATATAGC");
-        dnaList.add("GGCTATATATATGATATAGTAAC");
+        dnaList.add("GGCTATGATATATGATATAGTAAC");
         for (String dna : dnaList) {
             System.out.println("DNA strand is " + dna);
-            System.out.println(findSimpleGene(dna));
+            System.out.println(findSimpleGene(dna) + "\n");
         }
     }
 public static void main(String[] args) {
