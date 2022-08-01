37d5ee833deb05c9de31014835c6ea5f815f5c90
Piyush Acharya <acharyapiyush1@outlook.com>
2022-08-01 13:31:27 -0700

    Added ver 2

---
+++ StringsFirstAssignments/StringsFirstAssignments/Part2.java
@@ -0,0 +1,49 @@
+import java.util.*;
+
+public class Part2 {
+    public String findSimpleGene(String dna, String startCodon, String stopCodon) {
+        int startIndex = dna.indexOf("ATG");
+        if (startIndex == -1) {
+            return "Error: start codon (ATG) not found";
+        }
+        int stopIndex = dna.indexOf("TAA");
+        if (stopIndex == -1) {
+            return "Error: stop codon (TAA) not found";
+        }
+        if (((stopIndex - startIndex + 1) % 3) == 0) {
+            return ("Gene is " + dna.substring(startIndex, stopIndex + 3));
+        }
+        else {
+            return "Error: gene length is not a multiple of 3";
+        }
+    }
+
+    public void testSimpleGene() {
+        List<String> dnaList = new ArrayList<>();
+        dnaList.add("ATGGCTATATATAATATATAGC");
+        dnaList.add("ATGCATTAATTAATTAATAATGC");
+        dnaList.add("GCGCATGCATGCATGCTAAGC");
+        dnaList.add("GATGCCTAATGGCTAGCTATAAGCTGC");
+        dnaList.add("GCATGCGCGCGCGCGCTAAGCGGC");
+        dnaList.add("GCGCCGGCCATGGATCTAAGGCCGGCGC");
+        dnaList.add("GCTACATGGTACGTACGTAATAGC");
+        dnaList.add("GATGCTATATAATAGTTATATAGC");
+        dnaList.add("GGCTATGATATATGATATAGC");
+
+        ArrayList<String> startCodonListIsoleucine = new ArrayList<>(Arrays.asList("ATT", "ATC", "ATA", "b"));
+        System.out.println(startCodonListIsoleucine);
+
+        List<String> stopCodonList= new ArrayList<>();
+        stopCodonList.add("TAA");
+        stopCodonList.add("TAG");
+        stopCodonList.add("TGA");
+        
+
+        // ask user for the amino acid that they want to search for in the DNA strand, using the corresponding amino acid lists above.
+
+    }
+public static void main(String[] args) {
+        Part1 p = new Part1();
+        p.testSimpleGene();
+    }
+}
