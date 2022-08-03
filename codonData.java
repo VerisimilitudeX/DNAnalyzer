368dbf1253c96d6c48b550a3dcac9661fea51af9
Piyush Acharya <acharyapiyush1@outlook.com>
2022-08-02 20:53:16 -0700

    Refactored code further.

---
+++ codonData.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+public class codonData {
+    static ArrayList<String> Isoleucine = new ArrayList<>(Arrays.asList("ATT", "ATC", "ATA"));
+    static ArrayList<String> Leucine = new ArrayList<>(Arrays.asList("CTT", "CTC", "CTA", "CTG", "TTA", "TTG"));
+    static ArrayList<String> Valine = new ArrayList<>(Arrays.asList("GTT", "GTC", "GTA", "GTG"));
+    static ArrayList<String> Phenylalanine = new ArrayList<>(Arrays.asList("TTT", "TTC"));
+    static ArrayList<String> Methionine = new ArrayList<>(Arrays.asList("ATG"));
+    static ArrayList<String> Cysteine = new ArrayList<>(Arrays.asList("TGT", "TGC"));
+    static ArrayList<String> Alanine = new ArrayList<>(Arrays.asList("GCT", "GCC", "GCA", "GCG"));
+    static ArrayList<String> Glycine = new ArrayList<>(Arrays.asList("GGT", "GGC", "GGA", "GGG"));
+    static ArrayList<String> Proline = new ArrayList<>(Arrays.asList("CCT", "CCC", "CCA", "CCG"));
+    static ArrayList<String> Threonine = new ArrayList<>(Arrays.asList("ACT", "ACC", "ACA", "ACG"));
+    static ArrayList<String> Serine = new ArrayList<>(Arrays.asList("TCT", "TCC", "TCA", "TCG", "AGT", "AGC"));
+    static ArrayList<String> Tyrosine = new ArrayList<>(Arrays.asList("TAT", "TAC"));
+    static ArrayList<String> Tryptophan = new ArrayList<>(Arrays.asList("TGG"));
+    static ArrayList<String> Glutamine = new ArrayList<>(Arrays.asList("CAA", "CAG"));
+    static ArrayList<String> Asparagine = new ArrayList<>(Arrays.asList("AAT", "AAC"));
+    static ArrayList<String> Histidine = new ArrayList<>(Arrays.asList("CAT", "CAC"));
+    static ArrayList<String> GlutamicAcid = new ArrayList<>(Arrays.asList("GAA", "GAG"));
+    static ArrayList<String> AsparticAcid = new ArrayList<>(Arrays.asList("GAT", "GAC"));
+    static ArrayList<String> Lysine = new ArrayList<>(Arrays.asList("AAA", "AAG"));
+    static ArrayList<String> Arginine = new ArrayList<>(Arrays.asList("CGT", "CGC", "CGA", "CGG", "AGA", "AGG"));
+
+    static ArrayList<String> Stop = new ArrayList<>(Arrays.asList("TAA", "TAG", "TGA"));
+}
--- entryPoint.java
+++ entryPoint.java
@@ -1,32 +1,8 @@
-import java.util.*;
-
 public class entryPoint {
-    static ArrayList<String> Isoleucine = new ArrayList<>(Arrays.asList("ATT", "ATC", "ATA"));
-    static ArrayList<String> Leucine = new ArrayList<>(Arrays.asList("CTT", "CTC", "CTA", "CTG", "TTA", "TTG"));
-    static ArrayList<String> Valine = new ArrayList<>(Arrays.asList("GTT", "GTC", "GTA", "GTG"));
-    static ArrayList<String> Phenylalanine = new ArrayList<>(Arrays.asList("TTT", "TTC"));
-    static ArrayList<String> Methionine = new ArrayList<>(Arrays.asList("ATG"));
-    static ArrayList<String> Cysteine = new ArrayList<>(Arrays.asList("TGT", "TGC"));
-    static ArrayList<String> Alanine = new ArrayList<>(Arrays.asList("GCT", "GCC", "GCA", "GCG"));
-    static ArrayList<String> Glycine = new ArrayList<>(Arrays.asList("GGT", "GGC", "GGA", "GGG"));
-    static ArrayList<String> Proline = new ArrayList<>(Arrays.asList("CCT", "CCC", "CCA", "CCG"));
-    static ArrayList<String> Threonine = new ArrayList<>(Arrays.asList("ACT", "ACC", "ACA", "ACG"));
-    static ArrayList<String> Serine = new ArrayList<>(Arrays.asList("TCT", "TCC", "TCA", "TCG", "AGT", "AGC"));
-    static ArrayList<String> Tyrosine = new ArrayList<>(Arrays.asList("TAT", "TAC"));
-    static ArrayList<String> Tryptophan = new ArrayList<>(Arrays.asList("TGG"));
-    static ArrayList<String> Glutamine = new ArrayList<>(Arrays.asList("CAA", "CAG"));
-    static ArrayList<String> Asparagine = new ArrayList<>(Arrays.asList("AAT", "AAC"));
-    static ArrayList<String> Histidine = new ArrayList<>(Arrays.asList("CAT", "CAC"));
-    static ArrayList<String> GlutamicAcid = new ArrayList<>(Arrays.asList("GAA", "GAG"));
-    static ArrayList<String> AsparticAcid = new ArrayList<>(Arrays.asList("GAT", "GAC"));
-    static ArrayList<String> Lysine = new ArrayList<>(Arrays.asList("AAA", "AAG"));
-    static ArrayList<String> Arginine = new ArrayList<>(Arrays.asList("CGT", "CGC", "CGA", "CGG", "AGA", "AGG"));
-
-    static ArrayList<String> Stop = new ArrayList<>(Arrays.asList("TAA", "TAG", "TGA"));
-
+   
     public static void main(String[] args) {
         dnaSequencer ds = new dnaSequencer();
-        ds.getSequenceAndAminoAcid(Isoleucine, Leucine, Valine, Phenylalanine, Methionine, Cysteine, Alanine, Glycine, Proline, Threonine, Serine, Tyrosine, Tryptophan, Glutamine, Asparagine, Histidine, GlutamicAcid, AsparticAcid, Lysine, Arginine, Stop);
+        ds.getSequenceAndAminoAcid(codonData.Isoleucine, codonData.Leucine, codonData.Valine, codonData.Phenylalanine, codonData.Methionine, codonData.Cysteine, codonData.Alanine, codonData.Glycine, codonData.Proline, codonData.Threonine, codonData.Serine, codonData.Tyrosine, codonData.Tryptophan, codonData.Glutamine, codonData.Asparagine, codonData.Histidine, codonData.GlutamicAcid, codonData.AsparticAcid, codonData.Lysine, codonData.Arginine, codonData.Stop);
     }
     
 }
