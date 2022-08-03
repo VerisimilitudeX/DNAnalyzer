265139f36bea91db358bbdf9e91c386718821923
Piyush Acharya <acharyapiyush1@outlook.com>
2022-08-03 12:41:53 -0700

    Removed time tracking variables
    See commit history if bringing back the time variables is necessary.

--- entryPoint.java
+++ entryPoint.java
@@ -1,28 +1,11 @@
-// Imports the Java Time Library for measuring the time it takes for the program to run. 
-import java.time.Duration;
-import java.time.Instant;
-
 // Program starts here.
 public class entryPoint {
     public static void main(String[] args) {
-        
-        // Start measuring time
-        Instant start = Instant.now();
-        
-        // Create a new instance of the dnaSequence and codonData classes and send the information to the dnaSequencer's ds constructor.
-        int count = 0;
-        while (count < 1000) {
-            dnaSequencer ds = new dnaSequencer();
-            codonData cd = new codonData();
-            ds.getSequenceAndAminoAcid(cd.getIsoleucine(), cd.getLeucine(), cd.getValine(), cd.getPhenylalanine(), cd.getMethionine(), cd.getCysteine(), cd.getAlanine(), cd.getGlycine(), cd.getProline(), cd.getThreonine(), cd.getSerine(), cd.getTyrosine(), cd.getTryptophan(), cd.getGlutamine(), cd.getAsparagine(), cd.getHistidine(), cd.getGlutamicAcid(), cd.getAsparticAcid(), cd.getLysine(), cd.getArginine(), cd.getStop());
-            count++;
-        }
 
-        // Stop measuring time
-        findGeneWithAminoAcid.main(args);
-        Instant end = Instant.now();
-        Duration timeTaken = Duration.between(start, end);
-        System.out.println("Time taken: " + timeTaken.toMillis() + " milliseconds");
+        // Create a new instance of the dnaSequence and codonData classes and send the information to the dnaSequencer's ds constructor.
+        dnaSequencer ds = new dnaSequencer();
+        codonData cd = new codonData();
+        ds.getSequenceAndAminoAcid(cd.getIsoleucine(), cd.getLeucine(), cd.getValine(), cd.getPhenylalanine(), cd.getMethionine(), cd.getCysteine(), cd.getAlanine(), cd.getGlycine(), cd.getProline(), cd.getThreonine(), cd.getSerine(), cd.getTyrosine(), cd.getTryptophan(), cd.getGlutamine(), cd.getAsparagine(), cd.getHistidine(), cd.getGlutamicAcid(), cd.getAsparticAcid(), cd.getLysine(), cd.getArginine(), cd.getStop());
     }
 }
 
