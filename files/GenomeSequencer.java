1e7764e97651ec7585aed45a52edcff9925ca894
Piyush Acharya <acharyapiyush1@outlook.com>
2022-08-17 13:10:21 -0700

    Refactored the code

--- files/GenomeSequencer.java
+++ files/GenomeSequencer.java
@@ -1,8 +1,5 @@
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.nio.file.Files;
-import java.nio.file.Path;
 import java.util.ArrayList;
 import java.util.Scanner;
 
