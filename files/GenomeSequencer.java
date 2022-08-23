2b7a8262a6b1838ec21412a1fee37021d84dc0ef
Piyush Acharya <acharyapiyush1@outlook.com>
2022-08-23 14:03:14 -0700

    Removed timing calculations

--- files/GenomeSequencer.java
+++ files/GenomeSequencer.java
@@ -14,14 +14,12 @@ public class GenomeSequencer {
             ArrayList<String> Threonine, ArrayList<String> Serine, ArrayList<String> Tyrosine,
             ArrayList<String> Tryptophan, ArrayList<String> Glutamine, ArrayList<String> Asparagine,
             ArrayList<String> Histidine, ArrayList<String> GlutamicAcid, ArrayList<String> AsparticAcid,
-            ArrayList<String> Lysine, ArrayList<String> Arginine, ArrayList<String> Stop) throws IOException, InterruptedException {
+            ArrayList<String> Lysine, ArrayList<String> Arginine, ArrayList<String> Stop)
+            throws IOException, InterruptedException {
 
-        long startTime = System.currentTimeMillis();
         // Load DNA file and concatenate lines
-        String dna = Files.readString(Path.of("files/dna/teststop.fa")).replace("\n", "");
-        System.out.println("Load time: " + (System.currentTimeMillis() - startTime) + "ms");
+        String dna = Files.readString(Path.of("files/dna/dnalong.fa")).replace("\n", "").toLowerCase();
 
-        startTime = System.currentTimeMillis();
         // Checks if the DNA sequence is valid (contains only A, T, G, and C
         // nucleotides).
         if (dna.length() == 0) {
@@ -41,7 +39,6 @@ public class GenomeSequencer {
             System.out.println("Error: Invalid characters are present in DNA sequence.");
             return;
         }
-        System.out.println("Validate time: " + (System.currentTimeMillis() - startTime) + "ms");
 
         // Gets the amino acid from the user.
         Scanner userInput = new Scanner(System.in);
@@ -50,13 +47,10 @@ public class GenomeSequencer {
 
         userInput.close();
 
-        startTime = System.currentTimeMillis();
         // Prevents the user from entering an RNA sequence. In the last decade, using
         // DNA sequences instead of RNA sequences has been a more common practice.
         dna = dna.replace("u", "t");
-        System.out.println("Replace time: " + (System.currentTimeMillis() - startTime) + "ms");
 
-        startTime = System.currentTimeMillis();
         // Creates a new instance of the getAminoAcid class and sends the DNA, amino
         // acid, and start codons to the class.
         // Gets a StorageResource containing the genes of the amino acid.
@@ -71,35 +65,24 @@ public class GenomeSequencer {
 
         // Prints the list of amino acid genes found in the StorageResource object.
         p.printGeneList(geneList, aminoAcid);
-        System.out.println("Properties time: " + (System.currentTimeMillis() - startTime) + "ms");
 
-        startTime = System.currentTimeMillis();
         // Prints the GC-con tent of the genomic sequence.
         double gcContent = p.getGCContent(dna);
         System.out.println("\nGC-content (genome): " + gcContent);
-        System.out.println("GC-Content time: " + (System.currentTimeMillis() - startTime) + "ms");
 
-        startTime = System.currentTimeMillis();
         // Returns a HashMap containing the number of each nucleotide in the DNA
         // sequence.
         p.printNucleotideCount(dna);
-        System.out.println("Nucleotide count time: " + (System.currentTimeMillis() - startTime) + "ms");
 
-        startTime = System.currentTimeMillis();
         // Finds and prints GC-content higher than 0.35
         GeneInfo gi = new GeneInfo();
         gi.highGCContent(geneList);
-        System.out.println("High GC areas time: " + (System.currentTimeMillis() - startTime) + "ms");
 
-        startTime = System.currentTimeMillis();
         // Finds and prints the longest gene in the DNA sequence and its length.
         gi.longestGene(geneList);
         System.out.println();
-        System.out.println("Longest gene time: " + (System.currentTimeMillis() - startTime) + "ms");
 
-        startTime = System.currentTimeMillis();
         AminoAcidProperties aap = new AminoAcidProperties(dna, 1, 0, 5);
         aap.printCodonCounts();
-        System.out.println("AAP time: " + (System.currentTimeMillis() - startTime) + "ms");
     }
 }
