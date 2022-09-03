562dc6c7e4a64d9d0e4726a5ee3f00f1660bc5f9
Piyush Acharya <acharyapiyush1@outlook.com>
2022-09-02 21:27:45 -0700

    Updated var names

--- src/DNAnalyzer/Main.java
+++ src/DNAnalyzer/Main.java
@@ -8,19 +8,19 @@ public class Main {
 
         // Create a new instance of the dnaSequence and codonData classes and send the
         // information to the dnaSequencer's ds constructor.
-        final GenomeSequencer ds = new GenomeSequencer();
-        final CodonData scd = new CodonData();
-        ds.getSequenceAndAminoAcid(scd.getAminoAcid(AminoAcidNames.ISOLEUCINE),
-                scd.getAminoAcid(AminoAcidNames.LEUCINE),
-                scd.getAminoAcid(AminoAcidNames.VALINE), scd.getAminoAcid(AminoAcidNames.PHENYLALANINE),
-                scd.getAminoAcid(AminoAcidNames.METHIONINE), scd.getAminoAcid(AminoAcidNames.CYSTEINE),
-                scd.getAminoAcid(AminoAcidNames.ALANINE), scd.getAminoAcid(AminoAcidNames.GLYCINE),
-                scd.getAminoAcid(AminoAcidNames.PROLINE), scd.getAminoAcid(AminoAcidNames.THREONINE),
-                scd.getAminoAcid(AminoAcidNames.SERINE), scd.getAminoAcid(AminoAcidNames.TYROSINE),
-                scd.getAminoAcid(AminoAcidNames.TRYPTOPHAN), scd.getAminoAcid(AminoAcidNames.GLUTAMINE),
-                scd.getAminoAcid(AminoAcidNames.ASPARAGINE), scd.getAminoAcid(AminoAcidNames.HISTIDINE),
-                scd.getAminoAcid(AminoAcidNames.GLUTAMIC_ACID), scd.getAminoAcid(AminoAcidNames.ASPARTIC_ACID),
-                scd.getAminoAcid(AminoAcidNames.LYSINE), scd.getAminoAcid(AminoAcidNames.ARGININE),
-                scd.getAminoAcid(AminoAcidNames.STOP));
+        final GenomeSequencer gs = new GenomeSequencer();
+        final CodonData cd = new CodonData();
+        gs.getSequenceAndAminoAcid(cd.getAminoAcid(AminoAcidNames.ISOLEUCINE),
+                cd.getAminoAcid(AminoAcidNames.LEUCINE),
+                cd.getAminoAcid(AminoAcidNames.VALINE), cd.getAminoAcid(AminoAcidNames.PHENYLALANINE),
+                cd.getAminoAcid(AminoAcidNames.METHIONINE), cd.getAminoAcid(AminoAcidNames.CYSTEINE),
+                cd.getAminoAcid(AminoAcidNames.ALANINE), cd.getAminoAcid(AminoAcidNames.GLYCINE),
+                cd.getAminoAcid(AminoAcidNames.PROLINE), cd.getAminoAcid(AminoAcidNames.THREONINE),
+                cd.getAminoAcid(AminoAcidNames.SERINE), cd.getAminoAcid(AminoAcidNames.TYROSINE),
+                cd.getAminoAcid(AminoAcidNames.TRYPTOPHAN), cd.getAminoAcid(AminoAcidNames.GLUTAMINE),
+                cd.getAminoAcid(AminoAcidNames.ASPARAGINE), cd.getAminoAcid(AminoAcidNames.HISTIDINE),
+                cd.getAminoAcid(AminoAcidNames.GLUTAMIC_ACID), cd.getAminoAcid(AminoAcidNames.ASPARTIC_ACID),
+                cd.getAminoAcid(AminoAcidNames.LYSINE), cd.getAminoAcid(AminoAcidNames.ARGININE),
+                cd.getAminoAcid(AminoAcidNames.STOP));
     }
 }
