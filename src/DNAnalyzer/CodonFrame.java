80620d68bc55d34b56773b309886a29279ff1f89
shubhwip <shubhrjain7@gmail.com>
2022-10-02 10:50:06 +0530

    Refactor CodonFrame

--- src/DNAnalyzer/CodonFrame.java
+++ src/DNAnalyzer/CodonFrame.java
@@ -1,32 +1,55 @@
+/*
+ * Copyright © 2022 DNAnalyzer. Some rights reserved.
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+ *
+ * You are entirely responsible for the use of this application, including any and all activities that occur.
+ * While DNAnalyzer strives to fix all major bugs that may be either reported by a user or discovered while debugging,
+ * they will not be held liable for any loss that the user may incur as a result of using this application, under any circumstances.
+ *
+ * For further inquiries, please contact DNAnalyzer@piyushacharya.com
+ */
+
 package DNAnalyzer;
 
 import java.util.Objects;
 
-public class CodonFrame {
-    private final String dna;
-    private final short readingFrame;
-    private final int min;
-    private final int max;
+/**
+ * Declares the codon frame
+ *
+ * @author Shubham Jain (@shubhwip)
+ * @version 1.2.1
+ * @see "https://en.wikipedia.org/wiki/DNA_and_RNA_codon_tables"
+ */
+public record CodonFrame(String dna, short readingFrame, int min, int max) {
 
-    public CodonFrame(String dna, short readingFrame, int min, int max) {
-        this.dna = dna;
-        this.readingFrame = readingFrame;
-        this.min = min;
-        this.max = max;
-    }
-
+    /**
+     * gets dna
+     * @return dna
+     */
     public String getDna() {
         return dna;
     }
 
+    /**
+     * gets reading frame
+     * @return reading frame
+     */
     public short getReadingFrame() {
         return readingFrame;
     }
 
+    /**
+     * gets min
+     * @return min
+     */
     public int getMin() {
         return min;
     }
 
+    /**
+     * gets max
+     * @return max
+     */
     public int getMax() {
         return max;
     }
