fc6c07a83b43342a39e42da23dc8b3fe85eeae65
plopez26DPU <plopez26@depaul.edu>
2022-10-17 18:27:44 -0500

    Move Codon files and refactor as needed

---
+++ src/main/java/DNAnalyzer/codon/CodonDataUtils.java
@@ -0,0 +1,35 @@
+/*
+ * Copyright © 2022 DNAnalyzer. Some rights reserved.
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+ *
+ * You are entirely responsible for the use of this application, including any and all activities that occur.
+ * While DNAnalyzer strives to fix all major bugs that may be either reported by a user or discovered while debugging,
+ * they will not be held liable for any loss that the user may incur as a result of using this application, under any circumstances.
+ *
+ * For further inquiries, please contact DNAnalyzer@piyushacharya.com
+ */
+
+package DNAnalyzer.codon;
+
+import DNAnalyzer.aminoAcid.*;
+import java.util.List;
+
+/**
+ * provide function to access codon data from amino acid.
+ *
+ * @author Piyush Acharya (@Verisimilitude11)
+ * @version 1.2.1
+ * @see "https://en.wikipedia.org/wiki/DNA_and_RNA_codon_tables"
+ */
+public class CodonDataUtils {
+
+    /**
+     * Returns the codon data for the amino acid.
+     *
+     * @param name The name of the amino acid.
+     * @return A List of codon data for the user-selected amino acid or stop codon.
+     */
+    public static List<String> getAminoAcid(final AminoAcidNames name) {
+        return CodonDataConstants.CodonDataAcidMap.getOrDefault(name, List.of());
+    }
+}
---
+++ src/main/java/DNAnalyzer/codon/CodonFrame.java
@@ -0,0 +1,79 @@
+/*
+ * Copyright © 2022 DNAnalyzer. Some rights reserved.
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+ *
+ * You are entirely responsible for the use of this application, including any and all activities that occur.
+ * While DNAnalyzer strives to fix all major bugs that may be either reported by a user or discovered while debugging,
+ * they will not be held liable for any loss that the user may incur as a result of using this application, under any circumstances.
+ *
+ * For further inquiries, please contact DNAnalyzer@piyushacharya.com
+ */
+
+package DNAnalyzer.codon;
+
+import DNAnalyzer.codon.*;
+import DNAnalyzer.protein.*;
+import DNAnalyzer.aminoAcid.*;
+import java.util.Objects;
+
+/**
+ * Declares the codon frame
+ *
+ * @author Shubham Jain (@shubhwip)
+ * @version 1.2.1
+ * @see "https://en.wikipedia.org/wiki/DNA_and_RNA_codon_tables"
+ */
+public record CodonFrame(String dna, short readingFrame, int min, int max) {
+
+    /**
+     * gets dna
+     *
+     * @return dna
+     */
+    public String getDna() {
+        return dna;
+    }
+
+    /**
+     * gets reading frame
+     *
+     * @return reading frame
+     */
+    public short getReadingFrame() {
+        return readingFrame;
+    }
+
+    /**
+     * gets min
+     *
+     * @return min
+     */
+    public int getMin() {
+        return min;
+    }
+
+    /**
+     * gets max
+     *
+     * @return max
+     */
+    public int getMax() {
+        return max;
+    }
+
+    @Override
+    public boolean equals(final Object o) {
+        if (this == o)
+            return true;
+        if (!(o instanceof CodonFrame))
+            return false;
+        final CodonFrame that = (CodonFrame) o;
+        return getReadingFrame() == that.getReadingFrame() && getMin() == that.getMin() && getMax() == that.getMax()
+                && getDna().equals(that.getDna());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getDna(), getReadingFrame(), getMin(), getMax());
+    }
+}
