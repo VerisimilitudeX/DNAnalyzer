e9e72e2e8d1d655462e74a6f3e6cd7612934020b
boaglio <boaglio@gmail.com>
2022-10-19 23:45:06 -0300

    fix: conflicting files

--- src/main/java/DNAnalyzer/CmdArgs.java
+++ src/main/java/DNAnalyzer/CmdArgs.java
@@ -10,142 +10,106 @@
  */
 package DNAnalyzer;
 
-import java.io.File;
-import java.io.IOException;
-import java.nio.file.Files;
-import java.util.List;
-import java.util.Optional;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
-
-import DNAnalyzer.protein.ProteinAnalysis;
-import DNAnalyzer.protein.ProteinFinder;
-import DNAnalyzer.codon.CodonFrame;    
 import picocli.CommandLine.Command;
 import picocli.CommandLine.Option;
 import picocli.CommandLine.Parameters;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
 
 /**
  * Class for handling command-line arguments.
  *
  * @version 1.2.1
  */
-@Command(name = "DNAnalyzer", mixinStandardHelpOptions = true, description = "A program to analyze DNA sequences.")
+@Command(
+        name = "DNAnalyzer",
+        mixinStandardHelpOptions = true,
+        description = "A program to analyze DNA sequences.")
 public class CmdArgs implements Runnable {
-    private static final short READING_FRAME = 1;
-
-    @Option(required = true, names = { "--amino" }, description = "The amino acid representing the start of a gene.")
+    @Option(
+            required = true,
+            names = {"--amino"},
+            description = "The amino acid representing the start of a gene.")
     String aminoAcid;
 
-    @Option(names = { "--min" }, description = "The minimum count of the reading frame.")
+    @Option(
+            names = {"--min"},
+            description = "The minimum count of the reading frame.")
     int minCount = 0;
 
-    @Option(names = { "--max" }, description = "The maximum count of the reading frame.")
+    @Option(
+            names = {"--max"},
+            description = "The maximum count of the reading frame.")
     int maxCount = 0;
 
     @Parameters(paramLabel = "DNA", description = "The FASTA file to be analyzed.")
     File dnaFile;
 
-    @Option(names = { "--find" }, description = "The DNA sequence to be found within the FASTA file.")
+    @Option(
+            names = {"--find"},
+            description = "The DNA sequence to be found within the FASTA file.")
     File proteinFile;
 
-    @Option(names = { "--reverse", "-r" }, description = "Reverse the DNA sequence before processing.")
+    @Option(
+            names = {"--reverse", "-r"},
+            description = "Reverse the DNA sequence before processing.")
     boolean reverse;
 
     /**
-     * Output a list of proteins, GC content, Nucleotide content, and other
-     * information found in a DNA
-     * sequence.
+     * Reads the contents of a file, stripping out newlines and converting everything to lowercase.
      *
-     * @throws IllegalArgumentException
-     *             when the DNA FASTA file contains an invalid DNA sequence
+     * @param file the file to read
+     * @return String with the contents of the file (newlines removed and converted to lowercase)
+     * @throws IOException if there is an error reading the file
      */
-    @Override
-    public void run() {
-        try {
-            Main.clearTerminal();
-
-            final String dna = readDNA();
-            final List<String> proteins = ProteinFinder.getProtein(dna, aminoAcid);
-            // Output the proteins, GC content, and nucleotide cnt found in the DNA
-            Properties.printProteinList(proteins, aminoAcid);
-            System.out.println("\nGC-content (genome): " + Properties.getGCContent(dna) + "\n");
-            Properties.printNucleotideCount(dna);
-
-            // Output the number of codons based on the reading frame the user wants to look
-            // at, and minimum and maximum filters
-            final CodonFrame codonFrame = new CodonFrame(dna, READING_FRAME, minCount, maxCount);
-            final ReadingFrames aap = new ReadingFrames(codonFrame);
-            System.out.println();
-            aap.printCodonCounts();
-
-            // Find protein sequence in DNA if necessary
-            readProtein().ifPresent(pr -> findProtein(dna, pr));
-
-            // Find the longest protein in DNA
-            ProteinAnalysis.printLongestProtein(proteins);
-
-            // Print if DNA is random
-            if (Properties.isRandomDNA(dna)) {
-                System.out.println("\n" + dnaFile.getName() + " has been detected to be random.");
-            }
-        } catch (IOException | InterruptedException e) {
-            e.printStackTrace();
-        }
-    }
-
-    /**
-     * Reads the contents of a file, stripping out newlines and converting
-     * everything to lowercase.
-     *
-     * @param file
-     *            the file to read
-     * @throws IOException
-     *             if there is an error reading the file
-     * @return String with the contents of the file (newlines removed and converted
-     *         to lowercase)
-     */
-    private static String readFile(File file) throws IOException {
+    String readFile(final File file) throws IOException {
         return Files.readString(file.toPath()).replace("\n", "").toLowerCase();
     }
 
+  /**
+   * Output a list of proteins, GC content, Nucleotide content, and other information found in a DNA
+   * sequence.
+   *
+   * @throws IllegalArgumentException when the DNA FASTA file contains an invalid DNA sequence
+   */
+  @Override
+  public void run() {
+    DnaAnalyzer dnaAnalyzer = dnaAnalyzer(aminoAcid)
+      .isValidDna()
+      .replaceDNA("u", "t");
+
+    if (reverse) {
+      dnaAnalyzer = dnaAnalyzer.reverseDna();
+    }
+
+    dnaAnalyzer
+      .printProteins()
+      .outPutCodons(minCount, maxCount)
+      .printLongestProtein();
+
+    if (Properties.isRandomDNA(dnaAnalyzer.dna().getDna())) {
+        System.out.println("\n" + dnaFile.getName() + " has been detected to be random.");
+    }
+  }
+
     /**
-     * Find protein sequence in DNA and print to stdout its position.
-     *
-     * @param dna
-     *            The DNA string
-     * @param protein
-     *            The protein string
+     * @param aminoAcid representing the start of the gene
+     * @return DnaAnalyzer which provides functions to analyze the dnaFile, protein file and supplied aminoAcid
      */
-    private void findProtein(String dna, String protein) {
-        Matcher m = Pattern.compile(protein).matcher(dna);
-        if (m.find()) {
-            System.out.println("\nProtein sequence found at index " + m.start() + " in the DNA sequence.");
-        } else {
-            System.out.println("\nProtein sequence not found in the DNA sequence.");
-        }
+  private DnaAnalyzer dnaAnalyzer(String aminoAcid) {
+    try {
+      String protein = null;
+      Main.clearTerminal();
+      final String dna = readFile(dnaFile);
+      if (proteinFile != null) {
+        protein = readFile(proteinFile);
+      }
+      return new DnaAnalyzer(new Dna(dna), protein, aminoAcid);
+    } catch (IOException | InterruptedException e) {
+      e.printStackTrace();
+      return new DnaAnalyzer(null, null, aminoAcid);
     }
-
-    /** Read protein form the proteinFile */
-    private Optional<String> readProtein() throws IOException {
-        if (proteinFile == null) {
-            return Optional.empty();
-        }
-        return Optional.of(readFile(proteinFile));
-    }
-
-    /** Load and preprocess DNA data */
-    private String readDNA() throws IOException { // Valid DNA?
-        String dna = readFile(dnaFile);
-        if (!dna.matches("[atgc]+")) {
-            throw new IllegalArgumentException("Invalid characters present in DNA sequence.");
-        }
-        // Replace Uracil with Thymine (in case user entered RNA and not DNA)
-        dna = dna.replace("u", "t");
-
-        if (reverse) {
-            dna = new StringBuilder(dna).reverse().toString();
-        }
-        return dna;
-    }
+  }
 }
