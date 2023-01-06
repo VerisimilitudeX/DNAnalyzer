0d4e9566ad5db22ee46948edd6f980d3d7ad52f0
Rishav Raj <rishavraj1707music@gmail.com>
2023-01-06 18:19:42 +0530

    Boolean literal is redundant

--- src/main/java/DNAnalyzer/ui/cli/CmdArgs.java
+++ src/main/java/DNAnalyzer/ui/cli/CmdArgs.java
@@ -64,7 +64,7 @@ public class CmdArgs implements Runnable {
 	 */
 	@Override
 	public void run() {
-		if (startGUI == true) {
+		if (startGUI) {
 			String[] args = new String[0];
 			DNAnalyzerGUI.launchIt(args);
 		} else {
@@ -72,7 +72,7 @@ public class CmdArgs implements Runnable {
 					.isValidDna()
 					.replaceDNA("u", "t");
 
-			if (reverse == true) {
+			if (reverse) {
 				dnaAnalyzer = dnaAnalyzer.reverseDna();
 			}
 
