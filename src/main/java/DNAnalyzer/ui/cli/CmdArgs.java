2b4fac4290e71f57a6d680a89b7912d38b7e552d
Rishav Raj <97666287+Rishav1707@users.noreply.github.com>
2023-01-06 19:14:22 +0530

    Update CmdArgs.java
    Signed-off-by: Rishav Raj <97666287+Rishav1707@users.noreply.github.com>

--- src/main/java/DNAnalyzer/ui/cli/CmdArgs.java
+++ src/main/java/DNAnalyzer/ui/cli/CmdArgs.java
@@ -64,7 +64,7 @@ public class CmdArgs implements Runnable {
 	 */
 	@Override
 	public void run() {
-		if (startGUI) {
+		if (startGUI == true) {
 			String[] args = new String[0];
 			DNAnalyzerGUI.launchIt(args);
 		} else {
