f3010c66eae8c7690519fa3361661533049a5caa
Lyndsay Futrell <lyndsayfutrell@gmail.com>
2023-01-28 21:11:40 -0500

    added filler to background and intro page

--- web/new/src/components/Background.js
+++ web/new/src/components/Background.js
@@ -4,9 +4,7 @@ export default function Background() {
 
     return(
     <div className="wrap main--text">
-    <h1 className="main--text">Coming Soon!</h1>
-    <h2>Our page is a work in process, check back later!</h2>
-    <a href="/"><button className="button">Home</button></a>
+    <h1 className="main--text background">The human genome is composed of over 3 billion base pairs, making human analysis nearly impossible. Consequently, using powerful computational and statistical methods to decode the functional information hidden in DNA sequences are necessary. The genome is also extremely intricate and contains a plethora of data, which need to be organized and converted into analyzable data appropriately. Current analytical tools and software make it arduous for both geneticists and physicians to do so, thus restricting them from acquiring crucial information to better understand humans.</h1>
   </div>
 );
     }
--- web/new/src/components/Intro.js
+++ web/new/src/components/Intro.js
@@ -4,9 +4,9 @@ export default function Intro() {
 
 return(
   <div className="wrap main--text">
-    <h1 className="main--text">Coming Soon!</h1>
-    <h2>Our page is a work in process, check back later!</h2>
-    <a href="/"><button className="button">Home</button></a>
+    <div className="wrap main--text">
+      <h1 className="main--text background">DNAnalyzer identifies proteins, amino acids, start and stop codons, high coverage regions, regions susceptible to neurodevelopmental disorders, transcription factors, and regulatory elements. Researchers are working to extract valuable information from such software to better understand human health and disease. Currently, we have a Command-Line-Interface (CLI) and are working on a Graphical User Interface (GUI) that will enable physicians to quickly and more easily interact with the software, enabling them to identify genetic mutations that may cause disease.</h1>
+    </div>
   </div>
 );
 }
