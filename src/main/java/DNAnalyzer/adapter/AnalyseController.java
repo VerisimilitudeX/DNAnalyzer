b485ae5bb9e099676101ee484ebccc35d17bfc44
Martin Gallauner <martin.gallauner@pm.me>
2023-10-07 14:10:40 +0200

    removes mistakes in controller classes.

--- src/main/java/DNAnalyzer/adapter/AnalyseController.java
+++ src/main/java/DNAnalyzer/adapter/AnalyseController.java
@@ -1,12 +1,5 @@
 package DNAnalyzer.adapter;
 
-import DNAnalyzer.core.port.in.AnalyzeDnaRequest;
-import DNAnalyzer.core.port.in.AnalyzeDnaUseCase;
-import DNAnalyzer.core.port.in.AnalyzeResult;
+public class AnalyseController {
 
-public class AnalyseController implements AnalyzeDnaUseCase {
-    @Override
-    public AnalyzeResult analyzeDna(AnalyzeDnaRequest request) {
-        return null;
-    }
 }
--- src/main/java/DNAnalyzer/adapter/ApiKeyController.java
+++ src/main/java/DNAnalyzer/adapter/ApiKeyController.java
@@ -1,17 +1,6 @@
 package DNAnalyzer.adapter;
 
-import DNAnalyzer.core.port.in.GetApiKeyUseCase;
-import DNAnalyzer.core.port.in.SetApiKeyUseCase;
+public class ApiKeyController {
 
-public class ApiKeyController implements GetApiKeyUseCase, SetApiKeyUseCase {
 
-    @Override
-    public String getApiKey() {
-        return null;
-    }
-
-    @Override
-    public String setApiKey(String apiKey) {
-        return null;
-    }
 }
