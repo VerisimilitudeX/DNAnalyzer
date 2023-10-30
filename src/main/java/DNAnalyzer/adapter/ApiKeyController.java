69dcd52956ccc1875c14ce7757eebafb53bbc39c
Martin Gallauner <martin.gallauner@pm.me>
2023-10-30 12:07:11 +0100

    Resolving finding of code analysis

--- src/main/java/DNAnalyzer/adapter/ApiKeyController.java
+++ src/main/java/DNAnalyzer/adapter/ApiKeyController.java
@@ -2,10 +2,6 @@ package DNAnalyzer.adapter;
 
 import DNAnalyzer.core.port.in.GetApiKeyUseCase;
 import DNAnalyzer.core.port.in.SetApiKeyUseCase;
-import lombok.Getter;
-import lombok.RequiredArgsConstructor;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 /**
@@ -13,11 +9,11 @@ import org.springframework.web.bind.annotation.*;
  */
 @RestController
 @RequestMapping("/api-key")
-@Controller
 public class ApiKeyController {
 
     /**
      * Contructor of the controller.
+     *
      * @param getApiKeyUseCase the use case to get the API key
      * @param setApiKeyUseCase the use case to set the API key
      */
@@ -26,11 +22,12 @@ public class ApiKeyController {
         this.setApiKeyUseCase = setApiKeyUseCase;
     }
 
-    private final GetApiKeyUseCase getApiKeyUseCase;
-    private final SetApiKeyUseCase setApiKeyUseCase;
+    private GetApiKeyUseCase getApiKeyUseCase;
+    private SetApiKeyUseCase setApiKeyUseCase;
 
     /**
      * Get the API key.
+     *
      * @return the API key
      */
     @GetMapping
@@ -41,6 +38,7 @@ public class ApiKeyController {
 
     /**
      * Set the API key.
+     *
      * @param request the request containing the new API key
      * @return the new API key
      */
