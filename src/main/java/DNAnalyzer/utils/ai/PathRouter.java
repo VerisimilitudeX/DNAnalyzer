695ffdcc60fbdb97cd8ae5521bccecd0e9d29b7d
Piyush Acharya <acharyapiyush1@outlook.com>
2024-06-22 01:10:55 -0700

    Add more refined AI response powered by GPT 4o

--- src/main/java/DNAnalyzer/utils/ai/PathRouter.java
+++ src/main/java/DNAnalyzer/utils/ai/PathRouter.java
@@ -1,11 +1,5 @@
 package DNAnalyzer.utils.ai;
 
-import DNAnalyzer.core.DNAAnalysis;
-import DNAnalyzer.ui.cli.CmdArgs;
-import DNAnalyzer.ui.gui.DNAnalyzerGUI;
-import DNAnalyzer.ui.gui.DNAnalyzerGUIFXMLController;
-import DNAnalyzer.utils.core.DNATools;
-import DNAnalyzer.utils.core.Utils;
 import java.io.BufferedReader;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
@@ -14,9 +8,17 @@ import java.io.PrintStream;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.util.stream.Collectors;
+
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
+
+import DNAnalyzer.core.DNAAnalysis;
+import DNAnalyzer.ui.cli.CmdArgs;
+import DNAnalyzer.ui.gui.DNAnalyzerGUI;
+import DNAnalyzer.ui.gui.DNAnalyzerGUIFXMLController;
+import DNAnalyzer.utils.core.DNATools;
+import DNAnalyzer.utils.core.Utils;
 import picocli.CommandLine;
 
 /**
@@ -93,7 +95,7 @@ public class PathRouter {
               + " it, analyze it anyway - I won't judge if it's wrong so don't worry. DONT"
               + " SUMMARIZE THE OUTPUT PROVIDED AND DONT JUST INCLUDE THE NUMBERS. YOU HAVE TO"
               + " EXPLAIN THE SIGNIFICANCE OF EVERYTHING, provide the numbers as evidence, and also give actionable tasks to the user"
-              + " that they can do to get better/improve what they are lacking. Then, answer those questions."
+              + " that they can do to get better/improve what they are lacking. Then, answer those questions. Please format it to be outputted to the terminal (so no markdown). Do not include or repeat the text that is provided to you WITHOUT PROVIDING ADDITIONAL CONTEXT. Dont include the version number as the title. Do not use markdown or * symbols in the response as this will be outputted to the Terminal/command line."
               + output
               + "\"\n"
               + "        }\n"
