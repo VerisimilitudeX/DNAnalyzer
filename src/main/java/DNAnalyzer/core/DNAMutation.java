b5e9386a23f75ad502b36f1aac9bbde27cb9d9f7
Paucey <alexanderhagen@live.com>
2025-01-30 10:08:19 -0400

    Create a DNAMutation class to mutate DNA sequences

---
+++ src/main/java/DNAnalyzer/core/DNAMutation.java
@@ -0,0 +1,76 @@
+package DNAnalyzer.core;
+
+import java.util.Random;
+import java.util.ArrayList;
+import java.util.List;
+
+public class DNAMutation {
+
+    private static final char[] BASES = { 'A', 'T', 'C', 'G' };
+    private static Random random = new Random();
+
+    /**
+     * Generates a list of 10 mutated DNA sequences based on the specified number of
+     * base mutations
+     * and prints them to the console.
+     * 
+     * @param dnaString    Original DNA sequence to mutate
+     * @param numMutations The number of base mutations to apply to each mutated
+     *                     sequence
+     */
+    public static void generateMutatedSequences(String dnaString, int numMutations) {
+        int numSequences = 10;
+
+        for (int i = 0; i < numSequences; i++) {
+            System.out.println(mutate(dnaString, numMutations));
+        }
+    }
+
+    /**
+     * Mutates a DNA sequence with a specified number of random base mutations.
+     *
+     * @param dnaString    Original DNA sequence to mutate
+     * @param numMutations The number of mutations (substitutions) to apply to the
+     *                     sequence
+     * @return A new mutated DNA sequence
+     */
+    private static String mutate(String dnaString, int numMutations) {
+        StringBuilder mutatedDna = new StringBuilder(dnaString);
+
+        // Create a list of all possible positions
+        List<Integer> availablePositions = new ArrayList<>();
+        for (int i = 0; i < dnaString.length(); i++) {
+            availablePositions.add(i);
+        }
+
+        // Perform mutations
+        for (int i = 0; i < numMutations; i++) {
+            // Select a random position from the available positions
+            int randomPosition = random.nextInt(availablePositions.size());
+            int position = availablePositions.remove(randomPosition);
+
+            char originalBase = dnaString.charAt(position);
+            char mutatedBase = getRandomBase(originalBase);
+
+            mutatedDna.setCharAt(position, mutatedBase);
+        }
+
+        return mutatedDna.toString();
+    }
+
+    /**
+     * Returns a random base different from the original base.
+     *
+     * @param originalBase Original base to mutate from
+     * @return A mutated base (different from the original)
+     */
+    private static char getRandomBase(char originalBase) {
+        char newBase;
+
+        do {
+            newBase = BASES[random.nextInt(BASES.length)];
+        } while (newBase == originalBase);
+
+        return newBase;
+    }
+}
